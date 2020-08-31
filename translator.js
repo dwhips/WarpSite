f843b1ce087f1f9b099473a8a60df90698754fd3
DWhips <danielwhipple940@gmail.com>
2020-08-30 19:44:29 -0500

    Added dropdown to choose translation, also updates user translation when new translation is chosen

--- translator.js
+++ translator.js
@@ -1,8 +1,31 @@
 var trans_txt = document.getElementById("trans_txt");
 var user_txt = document.getElementById("user_txt");
+var lang = document.getElementById("translator");
+
+lang.addEventListener('click', function(event) {
+    UpdateTranslation()
+})
 
 function UpdateTranslation() {
     // console.log("Update Triggered");
     // console.log(user_txt.value);
-    trans_txt.innerHTML = user_txt.value;
+    // console.log(lang.value);
+    var text = Translate(user_txt.value);
+    trans_txt.innerHTML = text;
+}
+
+function Translate(txt) {
+    switch (lang.value) {
+        case 'normal':
+            console.log("In Normal");
+            return txt;
+            break;
+        case 'backwards':
+            console.log("In Backwards");
+            return txt + 'BACKWARS';
+            break;
+        default:
+            console.log("Check the lang.value is properly typed in the html or called in this script");
+    }
+    return txt;
 }
