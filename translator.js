894466de106705ea9f2feed8f330e505ef1266e4
DWhips <danielwhipple940@gmail.com>
2020-08-31 12:17:44 -0500

    Added copy button which copies translation to clipboard

--- translator.js
+++ translator.js
@@ -16,11 +16,8 @@ function UpdateTranslation() {
 
 function Translate(txt) {
     switch (lang.value) {
-        case 'normal':
-            return txt;
-            break;
         case 'backwards':
-            console.log("In Backwards");
+            // console.log("In Backwards");
             return Reverse(txt);
             break;
         case 'zalgo':
@@ -31,6 +28,9 @@ function Translate(txt) {
             break;
         case 'owo':
             return Owoify(txt);
+            break;
+        case 'crillify':
+            return Crillify(txt);
             break;
         default:
             console.log("Check the lang.value is properly typed in the html or called in this script");
@@ -77,7 +77,9 @@ function Wormify(pzalg) {
     var text = pzalg.split('');
 
     for (var i = 0; i < text.length; i++) {
-        text[i] += '\u0303' + '\u02F7' + '\u0334' + '\u0360';
+        if (i != '\n') {
+            text[i] += '\u0303' + '\u02F7' + '\u0334' + '\u0360';
+        }
     }
     //Putting back together
     pzalg = '';
@@ -93,4 +95,98 @@ function Owoify(txt) {
     txt = txt.replace(/L/g, 'W');
     txt = txt.replace(/R/g, 'W');
     return txt;
+}
+
+function Crillify(txt) {
+    cyrillic_translit = {
+        '\u0410': 'A',
+        '\u0430': 'a',
+        '\u0411': 'B',
+        '\u0431': 'b',
+        '\u0412': 'V',
+        '\u0432': 'v',
+        '\u0413': 'G',
+        '\u0433': 'g',
+        '\u0414': 'D',
+        '\u0434': 'd',
+        '\u0415': 'E',
+        '\u0435': 'e',
+        '\u0416': 'Zh',
+        '\u0436': 'zh',
+        '\u0417': 'Z',
+        '\u0437': 'z',
+        '\u0418': 'I',
+        '\u0438': 'i',
+        '\u0419': 'I',
+        '\u0439': 'i',
+        '\u041a': 'K',
+        '\u043a': 'k',
+        '\u041b': 'L',
+        '\u043b': 'l',
+        '\u041c': 'M',
+        '\u043c': 'm',
+        '\u041d': 'N',
+        '\u043d': 'n',
+        '\u041e': 'O',
+        '\u043e': 'o',
+        '\u041f': 'P',
+        '\u043f': 'p',
+        '\u0420': 'R',
+        '\u0440': 'r',
+        '\u0421': 'S',
+        '\u0441': 's',
+        '\u0422': 'T',
+        '\u0442': 't',
+        '\u0423': 'U',
+        '\u0443': 'u',
+        '\u0424': 'F',
+        '\u0444': 'f',
+        '\u0425': 'Kh',
+        '\u0445': 'kh',
+        '\u0426': 'Ts',
+        '\u0446': 'ts',
+        '\u0427': 'Ch',
+        '\u0447': 'ch',
+        '\u0428': 'Sh',
+        '\u0448': 'sh',
+        '\u0429': 'Shch',
+        '\u0449': 'shch',
+        '\u042a': '"',
+        '\u044a': '"',
+        '\u042b': 'Y',
+        '\u044b': 'y',
+        '\u042c': "'",
+        '\u044c': "'",
+        '\u042d': 'E',
+        '\u044d': 'e',
+        '\u042e': 'Iu',
+        '\u044e': 'iu',
+        '\u042f': 'Ia',
+        '\u044f': 'ia'
+    }
+    result = txt.replace(/[a-z]/gi, m => cyrillic_translit[m]);
+}
+
+function copyToClipboard() {
+    var text = trans_txt.textContent;
+
+    if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text);
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
 }
