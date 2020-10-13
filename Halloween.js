bd3f5a204ac2d7acc982976d22275edc73444bee
DWhips <danielwhipple940@gmail.com>
2020-10-12 22:45:59 -0500

    Now zipper slider rotates in an angle relative to the mouse

--- Halloween.js
+++ Halloween.js
@@ -21,10 +21,12 @@ const mouse = {
 class Zipper {
     constructor() {
         this.width = 10;
+        this.height = this.width * 4;
         this.draggable = false;
         this.max_x = canvas.width * .9;
         this.min_x = canvas.width * .1;;
         this.x_pos = canvas.width * .9;
+        this.slider_angle = 0;
     }
 }
 var zip = new Zipper();
@@ -68,8 +70,8 @@ canvas.ontouchstart = function (event) {
 function initZipper() {
     c.strokeStyle = "white";
     c.beginPath();
-    c.moveTo(canvas.width * .1, y_mid);
-    c.lineTo(canvas.width * .9, y_mid);
+    c.moveTo(zip.min_x, y_mid);
+    c.lineTo(zip.max_x, y_mid);
     c.stroke();
 
     // prolly mod this for dynamic sizes
@@ -81,41 +83,46 @@ function initZipper() {
 // once mouth opens i might need a zipper class that keeps track of each init pos
 function DrawZipTeeth(nZips, hash_height) {
     nZips += 2; // for extra width on edge
-    const zip_width = canvas.width * .8 / nZips;
+    const zip_width = (zip.max_x - zip.min_x) / nZips;
     var zip_x;
 
     c.strokeStyle = "white";
     c.lineWidth = 3;
     for (var iZip = 1; iZip < nZips; iZip++) {
-        zip_x = canvas.width * .1 + iZip * zip_width;
+        zip_x = zip.min_x + iZip * zip_width;
 
         c.beginPath();
         c.moveTo(zip_x, y_mid + hash_height / 2);
         c.lineTo(zip_x, y_mid - hash_height / 2);
         c.stroke();
     }
-
 }
 
 // TODO with no angle or x pos
 function DrawZipSlider(x_pos) {
     x_pos -= zip.width / 2;
     c.fillStyle = "white";
-    c.fillRect(x_pos, y_mid, zip.width, zip.width * 3);
 
-    const x_circle_center = x_pos + zip.width / 2; // will change with angle stuff
-    const y_circle_center = y_mid + zip.width * 3 + 3;
+    c.translate(zip.x_pos, y_mid)
+    c.rotate(zip.slider_angle);// * Math.PI/180);
+    // c.fillRect(x_pos, y_mid, zip.width, zip.width * 3);
+    c.fillRect(-zip.width / 2, 0, zip.width, zip.width * 3);
+    c.rotate(-zip.slider_angle);// * Math.PI/180);
+    c.translate(-zip.x_pos, -y_mid)
+
+    var coord = CordFromAngle(zip.slider_angle + 90 * Math.PI / 180, zip.height-6, zip.x_pos, y_mid)
+    // const x_circle_center = x_pos + zip.width / 2; // will change with angle stuff
+    // const y_circle_center = y_mid + zip.width * 3 + 3;
     c.beginPath();
-    c.arc(x_circle_center, y_circle_center, zip.width / 2, 0, 2 * Math.PI);
+    c.arc(coord[0], coord[1], zip.width / 2, 0, 2 * Math.PI);
     c.stroke();
-
 }
 
 // will need to account for the angle
 function TouchingSlider() {
     if ((mouse.x >= zip.x_pos - zip.width / 2) && (mouse.x <= zip.x_pos + zip.width / 2)) {
         // currently zipper height harcoded
-        if ((mouse.y >= y_mid) && (mouse.y <= y_mid + zip.width * 4)) {
+        if ((mouse.y >= y_mid) && (mouse.y <= y_mid + zip.height)) {
             // console.log(mouse.x + " , " + mouse.y);
             zip.draggable = true;
         }
@@ -125,19 +132,49 @@ function TouchingSlider() {
 
 // makes sure the slidder doesnt extend beyond zipper bounds
 function VerifyZipPos(x) {
-    if (x < zip.min_x) {
+    // for left bound      TODO need a little spice now angle is added
+    zip.slider_angle = Angle2Mouse(zip.x_pos, y_mid) + 90 * Math.PI / 180;
+    if (x < zip.min_x - zip.height) {
         zip.x_pos = zip.min_x;
     }
-    else if (x > zip.max_x) {
+    // for right bound
+    else if (x > zip.max_x + zip.height) {
         zip.x_pos = zip.max_x;
     }
+    // within moving angle of slider but not moving it
+    else if (Math.abs(mouse.x - zip.x_pos) <= zip.height) {
+        // and 90* cos i have zipper down not pointing right, so relative angle should be 90* not 0*
+        // zip.slider_angle = Angle2Mouse(zip.x_pos, y_mid) + 90 * Math.PI / 180;
+    }
     else {
-        zip.x_pos = mouse.x;
+        // zip.slider_angle = Angle2Mouse(zip.x_pos, y_mid) + 90 * Math.PI / 180;
+        if (mouse.x < zip.x_pos) {
+            zip.x_pos = mouse.x + zip.height;
+        }
+        else {
+            zip.x_pos = mouse.x - zip.height;
+        }
     }
 }
 
+function Dist2Mouse(x, y) {
+    const x1 = x - mouse.x;
+    const y1 = y - mouse.y;
+    return Math.sqrt(x1 * x1 + y1 * y1);
+}
+
+function Angle2Mouse(x, y) {
+    return Math.atan2(y - mouse.y, x - mouse.x);
+}
+
+function CordFromAngle(ang, dist, x1, y1) {
+    var x = Math.round(Math.cos(ang) * dist + x1);
+    var y = Math.round(Math.sin(ang) * dist + y1);
+    return [x, y];
+}
+
 function animate() {
-    console.log(zip.draggable);
+    // console.log(zip.draggable);
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     c.fillStyle = "white";
